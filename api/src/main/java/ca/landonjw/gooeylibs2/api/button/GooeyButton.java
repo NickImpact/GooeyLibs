a94f05f4fdea05b797e29ef2618576a1f3dcfe61
Spy <spy@cosmics.dev>
2024-05-03 18:43:29 -0700

    change api to use jakarta annotations as javax is not included in all jdks

--- api/src/main/java/ca/landonjw/gooeylibs2/api/button/GooeyButton.java
+++ api/src/main/java/ca/landonjw/gooeylibs2/api/button/GooeyButton.java
@@ -15,8 +15,8 @@ import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.item.component.ItemLore;
 import net.minecraft.world.item.component.Unbreakable;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
