b95b77f3e0ba28a4cee3f12061e751920cda02e2
Stampede <thestampede2011@gmail.com>
2024-05-05 12:52:44 -0500

    Remove outdated comment

--- api/src/main/java/ca/landonjw/gooeylibs2/api/button/FlagType.java
+++ api/src/main/java/ca/landonjw/gooeylibs2/api/button/FlagType.java
@@ -2,10 +2,6 @@ package ca.landonjw.gooeylibs2.api.button;
 
 public enum FlagType
 {
-    /**
-     * ENCHANTS only hides tool enchants, as book ones are classified as stored enchantments.
-     * EXTRAS hides multiple things, according to the minecraft wiki (https://minecraft.fandom.com/wiki/Tutorials/Command_NBT_tags)
-     */
     Hide_Tooltip,
     Enchantments,
     Attribute_Modifiers,
